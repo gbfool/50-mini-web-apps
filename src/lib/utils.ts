f6dd2d56c194e24dad77d949933f05b23a74a2fd
humblefoool <humbledevs@gmail.com>
2023-11-06 01:39:24 +0530

    expanding card done

---
+++ src/lib/utils.ts
@@ -0,0 +1,51 @@
+import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function constructMetadata({
+  title = "50 Mini web apps",
+  description = "Building 50 mini web apps with NextJs, Typescript and Tailwind",
+  image = "/vercel.svg",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@gbfool",
+    },
+    icons,
+    metadataBase: new URL("https://the-stooge.vercel.app"),
+    themeColor: "#FFF",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
